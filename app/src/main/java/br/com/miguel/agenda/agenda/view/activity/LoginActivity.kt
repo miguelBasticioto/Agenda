3494e97fad87413126cae17af3adad7b85ac004f
alunor17 <alunor17@Mac-mini-de-administrador-2.local>
2018-07-18 17:10:09 -0300

    Initial commit

---
+++ app/src/main/java/br/com/miguel/agenda/agenda/view/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package br.com.miguel.agenda.agenda.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.miguel.agenda.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
