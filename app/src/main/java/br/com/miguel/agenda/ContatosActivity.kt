0e7849b99dba6c427f3dcb44ad153ad2d11d972b
Miguel Basticioto Bento <miguel_basticioto@hotmail.com>
2018-07-19 19:18:52 -0300

    Mais um commit

---
+++ app/src/main/java/br/com/miguel/agenda/ContatosActivity.kt
@@ -0,0 +1,14 @@
+package br.com.miguel.agenda
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ContatosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contatos)
+
+
+    }
+}
